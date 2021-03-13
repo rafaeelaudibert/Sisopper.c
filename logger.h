8404e6289b6a55d7c27e45e2dd325de54dad00de
RafaAudibert - Deepin <rafaeelaudibert@gmail.com>
2021-03-13 13:35:21 -0300

    :sparkles: Add basic logger library

---
+++ logger.h
@@ -0,0 +1,125 @@
+#ifndef LOGGER_H
+#define LOGGER_H
+
+#include <stdarg.h>
+#include <stdio.h>
+#include <time.h>
+#include <unistd.h>
+
+#define ERROR_TEXT "🚨 [ERROR] (%s) "
+#define WARN_TEXT "⚠️  [WARN] (%s) "
+#define INFO_TEXT "ℹ️   [INFO] (%s) "
+#define DEBUG_TEXT "🐛 [DEBUG] (%s) "
+
+char *get_current_timestamp()
+{
+    size_t buffer_size = 900 * sizeof(char);
+
+    time_t now;
+    struct tm ts;
+    char *buf = (char *)malloc(buffer_size);
+
+    // Get current time
+    time(&now);
+    ts = *localtime(&now);
+    strftime(buf, buffer_size, "%d/%m/%Y %H:%M:%S", &ts);
+
+    return buf;
+}
+
+int logger_error(char *fmt, ...)
+{
+    int ret;
+    va_list myargs;
+
+    char *current_timestmap = get_current_timestamp();
+
+    /* Initialise the va_list variable with the ... after fmt */
+    va_start(myargs, fmt);
+
+    /* Forward the '...' to printf with the correct start */
+    printf(ERROR_TEXT, current_timestmap);
+    ret = vprintf(fmt, myargs);
+    printf("\n");
+
+    /* Clean up the va_list */
+    va_end(myargs);
+
+    // Free timestamp
+    free(current_timestmap);
+
+    return ret;
+}
+
+int logger_warn(char *fmt, ...)
+{
+    int ret;
+    va_list myargs;
+
+    char *current_timestmap = get_current_timestamp();
+
+    /* Initialise the va_list variable with the ... after fmt */
+    va_start(myargs, fmt);
+
+    /* Forward the '...' to printf with the correct start */
+    printf(WARN_TEXT, current_timestmap);
+    ret = vprintf(fmt, myargs);
+    printf("\n");
+
+    /* Clean up the va_list */
+    va_end(myargs);
+
+    // Free timestamp
+    free(current_timestmap);
+
+    return ret;
+}
+
+int logger_info(char *fmt, ...)
+{
+    int ret;
+    va_list myargs;
+
+    char *current_timestmap = get_current_timestamp();
+
+    /* Initialise the va_list variable with the ... after fmt */
+    va_start(myargs, fmt);
+
+    /* Forward the '...' to printf with the correct start */
+    printf(INFO_TEXT, current_timestmap);
+    ret = vprintf(fmt, myargs);
+    printf("\n");
+
+    /* Clean up the va_list */
+    va_end(myargs);
+
+    // Free timestamp
+    free(current_timestmap);
+
+    return ret;
+}
+
+int logger_debug(char *fmt, ...)
+{
+    int ret;
+    va_list myargs;
+
+    char *current_timestmap = get_current_timestamp();
+
+    /* Initialise the va_list variable with the ... after fmt */
+    va_start(myargs, fmt);
+
+    /* Forward the '...' to printf with the correct start */
+    printf(DEBUG_TEXT, current_timestmap);
+    ret = vprintf(fmt, myargs);
+    printf("\n");
+
+    /* Clean up the va_list */
+    va_end(myargs);
+
+    // Free timestamp
+    free(current_timestmap);
+
+    return ret;
+}
+#endif // LOGGER_H
