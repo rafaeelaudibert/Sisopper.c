bc586cf6fec1a6d143701be874b6dc3df8df6968
RafaAudibert - Deepin <rafaeelaudibert@gmail.com>
2021-03-13 19:50:44 -0300

    :recycle: Update server error handler

--- server.c
+++ server.c
@@ -20,7 +20,7 @@
 
 #define BUFFER_SIZE 5
 
-static int should_listen_for_connections = TRUE;
+static int received_sigint = FALSE;
 
 void *handle_connection(void *);
 void close_socket(void *);
@@ -34,9 +34,11 @@ CHAINED_LIST *chained_list_threads = NULL;
 
 int main(int argc, char *argv[])
 {
+    logger_debug("Initializing on debug mode!\n");
+
     handle_signals();
 
-    int sockfd;
+    int sockfd, exit_code = 0;
     socklen_t clilen = sizeof(struct sockaddr_in);
     struct sockaddr_in serv_addr, cli_addr;
 
@@ -54,32 +56,51 @@ int main(int argc, char *argv[])
     if (bind(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
     {
         logger_error("On binding socket\n");
-        exit(ERROR_BINDING_SOCKET);
+        exit_code = ERROR_BINDING_SOCKET;
+        goto cleanup;
     }
 
-    listen(sockfd, CONNECTIONS_TO_ACCEPT);
+    if (listen(sockfd, CONNECTIONS_TO_ACCEPT) < 0)
+    {
+        logger_error("On starting to listen");
+        exit_code = ERROR_LISTEN;
+        goto cleanup;
+    }
     logger_info("Listening on port %d...\n", PORT);
 
-    while (should_listen_for_connections)
+    while (!received_sigint)
     {
         int *newsockfd = (int *)malloc(sizeof(int));
-        if ((*newsockfd = accept(sockfd, (struct sockaddr *)&cli_addr, &clilen)) == -1)
+        *newsockfd = accept(sockfd, (struct sockaddr *)&cli_addr, &clilen);
+
+        // If received a sigint while it was blocked waiting for a new connection, we skip what we are doing below
+        if (received_sigint)
+            break;
+
+        if (*newsockfd == -1)
+        {
             logger_error("On accept\n");
-        logger_info("New connection from %s:%d | %d\n", inet_ntoa(cli_addr.sin_addr), cli_addr.sin_port, cli_addr.sin_family);
+            exit_code = ERROR_ACCEPT;
+            goto cleanup;
+        }
+
+        logger_info("New connection from %s:%d\n", inet_ntoa(cli_addr.sin_addr), cli_addr.sin_port);
 
         pthread_t *thread = (pthread_t *)malloc(sizeof(pthread_t));
         pthread_create(thread, NULL, (void *(*)(void *)) & handle_connection, (void *)newsockfd);
-        logger_info("Created new thread to handle this connection\n");
+        logger_debug("Created new thread to handle this connection\n");
 
         chained_list_append_end(chained_list_sockets_fd, (void *)newsockfd);
         chained_list_append_end(chained_list_threads, (void *)thread);
     }
 
+cleanup:
     chained_list_iterate(chained_list_sockets_fd, &close_socket);
     chained_list_iterate(chained_list_threads, &join_thread);
+
     close(sockfd);
 
-    return 0;
+    return exit_code;
 }
 
 void *handle_connection(void *void_sockfd)
@@ -87,20 +108,20 @@ void *handle_connection(void *void_sockfd)
     char buffer[BUFFER_SIZE];
     int n, sockfd = *((int *)void_sockfd);
 
-    logger_info("[Socket %d] Hello from new thread to handle connection\n", sockfd);
-    while (should_listen_for_connections)
+    logger_debug("[Socket %d] Hello from new thread to handle connection\n", sockfd);
+    while (!received_sigint)
     {
         bzero(buffer, BUFFER_SIZE);
 
         /* read from the socket */
         n = read(sockfd, buffer, 256);
-        if (n < 0 && should_listen_for_connections)
+        if (n < 0 && !received_sigint)
         {
             logger_error("[Socket %d] On reading from socket\n", sockfd);
         }
         else if (n == 0)
         {
-            logger_warn("[Socket %d] Connection closed\n", sockfd);
+            logger_info("[Socket %d] Connection closed\n", sockfd);
             return NULL;
         }
         else
@@ -120,19 +141,26 @@ void *handle_connection(void *void_sockfd)
 void close_socket(void *void_socket)
 {
     int socket = *((int *)void_socket);
+
+    logger_debug("Closing socket %d...\n", socket);
     close(socket);
 }
 
 void join_thread(void *void_pthread)
 {
     pthread_t thread_id = *((pthread_t *)void_pthread);
+
+    logger_debug("Joining thread PID %d...\n", thread_id);
     pthread_join(thread_id, NULL);
 }
 
 void sigint_handler(int _sigint)
 {
-    logger_warn("SIGINT received, closing descriptors and finishing...\n");
-    should_listen_for_connections = FALSE;
+    if (!received_sigint)
+    {
+        logger_warn("SIGINT received, closing descriptors and finishing...\n");
+        received_sigint = TRUE;
+    }
 }
 
 void handle_signals(void)
@@ -144,7 +172,7 @@ void handle_signals(void)
     // Create a different thread to handle CTRL + D
     pthread_t *thread = (pthread_t *)malloc(sizeof(pthread_t));
     pthread_create(thread, NULL, (void *(*)(void *)) & handle_eof, (void *)NULL);
-    logger_info("Created new thread to handle EOF detection\n");
+    logger_debug("Created new thread to handle EOF detection\n");
 
     chained_list_append_end(chained_list_threads, (void *)thread);
 }
