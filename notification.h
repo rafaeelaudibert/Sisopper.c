6e0186faee26bd70c7213581309d752293e7d70b
RafaAudibert - Deepin <rafaeelaudibert@gmail.com>
2021-03-13 22:10:10 -0300

    :recycle: Update notification and packet to better contain the message

--- notification.h
+++ notification.h
@@ -4,13 +4,15 @@
 #include <stdint.h>
 #include <time.h>
 
+#include "config.h"
+
 typedef struct __notification
 {
-    uint32_t id;         // Identificador da notificação (sugere-se um identificador único)
-    time_t timestamp;    // Timestamp da notificação
-    uint8_t pending;     // Quantidade de leitores pendentes
-    uint16_t length;     // Tamanho da mensagem
-    const char *message; // Mensagem
+    uint32_t id;                        // Identificador da notificação (sugere-se um identificador único)
+    time_t timestamp;                   // Timestamp da notificação
+    uint8_t pending;                    // Quantidade de leitores pendentes
+    uint16_t length;                    // Tamanho da mensagem
+    char message[MAX_MESSAGE_SIZE + 2]; // Dados da mensagem
 } NOTIFICATION;
 
 #endif // NOTIFICATION_H
--- packet.h
+++ packet.h
@@ -4,12 +4,14 @@
 #include <stdint.h>
 #include <time.h>
 
+#include "config.h"
+
 typedef struct __packet
 {
-    uint16_t seqn;       // Número de sequência
-    uint16_t length;     // Comprimento do payload
-    time_t timestamp;    // Timestamp do dado
-    const char *payload; // Dados da mensagem
+    uint16_t seqn;                      // Número de sequência
+    uint16_t length;                    // Comprimento do payload
+    time_t timestamp;                   // Timestamp do dado
+    char payload[MAX_MESSAGE_SIZE + 2]; // Dados da mensagem
 } PACKET;
 
 #endif // PACKET_H
