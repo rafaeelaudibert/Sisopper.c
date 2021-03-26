cc83a333c05a3a4d6612d2a8478b37c2d47c242a
RafaAudibert - Deepin <rafaeelaudibert@gmail.com>
2021-03-26 02:39:01 +0100

    :recycle: Update notification to contain enough information

--- notification.h
+++ notification.h
@@ -6,13 +6,21 @@
 
 #include "config.h"
 
+typedef enum
+{
+    MESSAGE,
+    ERROR,
+} NOTIFICATION_TYPE;
+
 typedef struct __notification
 {
-    uint32_t id;                        // Identificador da notificação (sugere-se um identificador único)
-    time_t timestamp;                   // Timestamp da notificação
-    uint8_t pending;                    // Quantidade de leitores pendentes
-    uint16_t length;                    // Tamanho da mensagem
-    char message[MAX_MESSAGE_SIZE + 2]; // Dados da mensagem
+    uint32_t id;                          // Identificador da notificação (sugere-se um identificador único)
+    time_t timestamp;                     // Timestamp da notificação
+    uint8_t pending;                      // Quantidade de leitores pendentes
+    uint16_t length;                      // Tamanho da mensagem
+    NOTIFICATION_TYPE type;               // Tipo da notificação, para saber como mostrar na tela
+    char message[MAX_MESSAGE_SIZE + 2];   // Dados da mensagem
+    char author[MAX_USERNAME_LENGTH + 2]; // Nome do autor da mensagem
 } NOTIFICATION;
 
 #endif // NOTIFICATION_H
