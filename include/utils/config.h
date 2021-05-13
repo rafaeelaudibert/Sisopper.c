df7d601af3135cbff8326f598e9ea4447c52c5b1
Ana <acpagnoncelli@inf.ufrgs.br>
2021-05-12 23:14:58 -0300

    🐛 Fix missing comma

--- include/utils/config.h
+++ include/utils/config.h
@@ -18,7 +18,7 @@
 #define NUMBER_OF_CHARS_IN_FOLLOW 7
 
 int FE_PORTS[] = {12001, 12002, 12003, 12004, 12005};
-char *FE_HOSTS[] = {"127.0.0.1", "127.0.0.1", "127.0.0.1", "127.0.0.1", "127.0.0.1"}
+char *FE_HOSTS[] = {"127.0.0.1", "127.0.0.1", "127.0.0.1", "127.0.0.1", "127.0.0.1"};
 #define NUMBER_OF_FES (sizeof(FE_PORTS) / sizeof(FE_PORTS[0]))
 
 #endif // CONFIG_H
